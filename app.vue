7b787d56dc48631c206cbfd1cf7c8a162ed58743
= <91765010+ariIndu@users.noreply.github.com>
2024-04-17 17:22:13 -0500

    Fixed Faculty form, App.vue, and Dropdown

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<<<<<<< Updated upstream
+=======
+
+import ButtonCompoent from './components/Button.vue';
+<script setup>
+import Button from './components/Button.vue';
+</script>
+>>>>>>> Stashed changes
 <template>
   <div>
     <NuxtWelcome />
