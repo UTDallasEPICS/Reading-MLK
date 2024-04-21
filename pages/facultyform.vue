c6d330ac81553ca400b010c7e713ff99fb990350
Duc Nguyen <ductrungnguyen20034@gmail.com>
2024-04-21 11:31:55 -0500

    reworked submission and invoke childentry form

--- pages/facultyform.vue
+++ pages/facultyform.vue
@@ -23,7 +23,7 @@ const submitFaculty = async () =>{
 
 <template lang = "pug">
 MLKContainer 
-    div 
+    .flex.flex-col.gap-5 
         TitleDisplay Faculty Registration Form
         br
         .py-4.grid(class="sm:grid-cols-3")
@@ -41,17 +41,25 @@ MLKContainer
         .py-4.grid(class="sm:grid-cols-3")
             Label Faculty Email:
             .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
-                Input(v-model='data_FacultyProfile.faculty_email' placeholder="user defined" required)
+                Input(v-model='data_FacultyProfile.faculty_email' placeholder="(user defined)" required)
         .py-4.grid(class="sm:grid-cols-3")
             Label Do you teach a dual language class?
             .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
                 Dropdown(v-model="data_StudentProfile.dual_lang" :options="['True', 'False']" required) 
         .py-4.grid(class="sm:grid-cols-3")
-            Label 
-
-
-
-
-
-
+            Label School Name:
+            .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
+                Input(v-model='data_FacultyProfile.school_name' placeholder="user defined" required)
+        .py-4.grid(class="sm:grid-cols-3")
+            Label Department:
+            .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
+                Input(v-model='data_FacultyProfile.department' placeholder="(Ex: English, Science, or Math)" required)
+        .py-4.grid(class="sm:grid-cols-3")
+            Label Phone number:
+            .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
+                Input(v-model='data_FacultyProfile.phone_number' placeholder="(Ex: 1234567891)" required)
+        .py-4.grid(class="sm:grid-cols-3")
+            Label Grade you teach:
+            .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
+                Input(v-model='data_FacultyProfile.grade' placeholder="Ex: First, Second, or Pre-K" required)
 </template>
--- pages/parentform.vue
+++ pages/parentform.vue
@@ -16,7 +16,7 @@ gender: "",
 martial_status: ""
 });
 
-const data_StudentProfile = ref ([{
+const data_StudentProfile = ref([{
 first_name: "",
 last_name: "",
 pref_name: "",
@@ -48,38 +48,34 @@ pref_lang: ""
 })
 }
 
-const AddChildForm = ref(false);
-
-const toggleChildEntryForm = () => {
-  AddChildForm.value = !AddChildForm.value;
-};
+const removeStudent = (i: number) => {
+    data_StudentProfile.value.splice(i, 1);
+}
 
 const submittAccounts = async() =>{
-    // submitt for a parent account
     const parentResponse = await $fetch('/api/person', {
     method: "POST",
     body: data_ParentProfile.value
     });
-    // submitt for a student account array 
-    const studentResponses = await Promise.all(
-      data_StudentProfile.value.map(async (student) => {
-        return await $fetch('/api/person', {
-          method: 'POST',
-          body: student,
-        });
-      })
-    );
+    const studentResponse = data_StudentProfile.value.map(async (student) => {$fetch('/api/person', {
+        method: 'POST',
+        body: student
+    });
+    })
 
+    const studentForms = await Promise.all(studentResponse);
 
+    const combinedForm = [parentResponse, ...studentForms];
 };
+
+
 </script>
 
 
 <template lang = "pug">
 Container 
-    div 
+    .flex.flex-col.gap-5 
         TitleDisplay Parent Registration Form
-        br
         .py-4.grid(class="sm:grid-cols-3")
             Label First Name:
             .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
@@ -107,7 +103,7 @@ Container
         .py-4.grid(class="sm:grid-cols-3")
             Label Password:
             .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
-                Input(v-model='data_ParentProfile.password' placeholder="Please have at least one lower and upper case letter, a number, and a special character" required )
+                Input(v-model='data_ParentProfile.password' placeholder="(Please have at least one lower and upper case letter, a number, and a special character)" required )
         .py-4.grid(class="sm:grid-cols-3")
             Label Social Media Handle: (This is not required)
             .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
@@ -115,22 +111,18 @@ Container
         .py-4.grid(class="sm:grid-cols-3")
             Label On average, how many books do you read per year to your child? (A guess is fine)
             .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
-                Input(v-model='data_ParentProfile.avg_num_book' placeholder="If unsure, please give the lower boundary of how many books you read to your child" required)
+                Input(v-model='data_ParentProfile.avg_num_book' placeholder="(If unsure, please give the lower boundary of how many books you read to your child)" required)
         .py-4.grid(class="sm:grid-cols-3")
-            Label What is your marital status? (If you do not wish to answer or can't find an answer which matches your situation select other)
+            Label What is your marital status? (If you do not wish to answer or can't find an answer which matches your situation select Wish not to Disclose or Other respectively)
             .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
-                Dropdown(v-model="data_StudentProfile.gender" :options="['Married', 'Divorced', 'Single', 'Other', 'Wish not to Disclose']") 
-    div
+                Dropdown(v-model="data_StudentProfile.martial_status" :options="['Married', 'Divorced', 'Single', 'Other', 'Wish not to Disclose']") 
+    .flex.flex-col.gap-5
         TitleDisplay Child Registration Section
-        br 
-        .py-4.grid(class="sm:grid-cols-3")
-            Label Click here to add child form 
-            .col-md-9.mx-10(class="sm:col-span-2 sm:mr-11")
-                Button.mx-auto.text-md(name = "Add Child" @click="toggleChildEntryForm" class= "transition duration-500 bg-sky-600 hover: bg-green-400 ") Add a child
-            ChildEntry(v-if="AddChildForm")
-        .py-4.grid(class="sm:grid-cols-3")
-            Button.mx-auto.text-md(name="Submitt all accounts" @click= "submittAccounts" class="transition duration-500 bg-sky-600 hover: bg-green-400") Submitt forms
-
-
-
+    .flex.flex-col.gap-5
+        ChildEntry(v-for="(child, i), data_StudentProfile", v-model= "data_StudentProfile[i]" @remove="remove(i)")   
+        Button.mx-auto.text-md(name = "Add Child" @click="addStudent" class="transition duration-500 bg-sky-600 hover: bg-green-400") Add Student
+        
+        
+.py-4.grid(class="sm:grid-cols-3")
+    Button.mx-auto.text-md(name="Submitt accounts" @click= "submittAccounts" class="transition duration-500 bg-sky-600 hover: bg-green-400") Submitt
 </template>
