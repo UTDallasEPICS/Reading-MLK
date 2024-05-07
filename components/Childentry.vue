219240ddefa7b0a4f418151b82b18dc293d1b63d
Duc-T-Nguyen <122585425+Duc-T-Nguyen@users.noreply.github.com>
2024-05-07 12:57:18 -0500

    Update Childentry.vue

--- components/Childentry.vue
+++ components/Childentry.vue
@@ -123,7 +123,7 @@ div
                     Inputnum(type="number" v-model='reading_lvl' name="reading_lvl" id="reading_lvl" placeholder="(give number)" required)
             .py-4.grid(class="" )
                 Label * Birth date of child: 
-                    Input(type="date" v-model="birth_date"  @update:modelValue="handleBirthDateChange" name="date" id="date" placeholder="(user defined)" required)
+                    Input(type="date" v-model="birth_date" name="date" id="date" placeholder="(user defined)" required)
             .py-4.grid(class="" )
                 label * Gender:
                     select(v-model='gender' name="gender" id="gender" placeholder = "Please Select your Gender" class="block w-full rounded-md border border-gray-700 focus:ring-indigo-500"  required)
