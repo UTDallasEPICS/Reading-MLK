bf33c022b26317e2b971f24874182284d62d4b05
Jonathan Kopuri <kjernest01@gmail.com>
2024-11-09 09:07:53 -0600

    updated navbar styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
       "./error.vue",
     ],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          customBlue: '#122C4F',
+        }
+      },
     },
     plugins: [],
   }
