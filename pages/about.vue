c11fc0d5044d603c30088797a71ebb732e272745
Jonathan Kopuri <kjernest01@gmail.com>
2024-11-13 14:39:18 -0600

    made an about page

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+    h2(class="text-4xl sm:text-2xl lg:text-4xl font-semibold text-gray-800 hover:text-yellow-500 transition-all duration-300 cursor-pointer") About Page
+  </template>
+  
+  <script setup>
+  import { ref, reactive } from 'vue';
+  </script>
