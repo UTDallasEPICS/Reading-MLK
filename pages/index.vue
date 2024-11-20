b4b4a04b342d96716aa841b7f23189cd34972211
Jonathan Kopuri <kjernest01@gmail.com>
2024-11-20 16:25:50 -0600

    Responsive Design

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,41 @@
   .main-container(class="flex flex-col items-center justify-center bg-gray-100 py-10")
     .centered-container(class="flex items-center justify-center text-center my-10 sm:mx-10 lg:mx-20 cursor-default")
       h1(class="text-4xl sm:text-4xl lg:text-7xl font-extrabold text-gray-800") Welcome to Reading Huddle
+
+    //- .module-container(class="flex flex-wrap justify-center items-center p-6 sm:p-8 rounded-2xl bg-[#FFCD98] w-[90vw] sm:w-auto sm:gap-36 shadow-lg")
+    //-   .module(
+    //-     :key="index"
+    //-     class="flex items-center p-4 sm:p-5 bg-white rounded-xl shadow-xl transition-transform transform hover:scale-105 duration-300 w-full sm:w-fit mb-6 sm:mb-0"
+    //-   )
+    //-     img(src="/home.svg" alt="Reading Huddle Logo" class="w-16 h-16 mr-4 rounded-full object-contain cursor-pointer")
+    //-     h2(class="text-2xl sm:text-4xl font-semibold text-gray-800 hover:text-yellow-500 transition-all duration-300 cursor-pointer text-ellipsis overflow-hidden whitespace-nowrap") About Us
+    //-   .module(
+    //-     :key="index"
+    //-     class="flex items-center p-4 sm:p-5 bg-white rounded-xl shadow-xl transition-transform transform hover:scale-105 duration-300 w-full sm:w-fit"
+    //-   )
+    //-     img(src="/bookstack.svg" alt="Reading Huddle Logo" class="w-16 h-16 mr-4 rounded-full object-contain")
+    //-     h2(class="text-2xl sm:text-4xl font-semibold text-gray-800 hover:text-yellow-500 transition-all duration-300 cursor-pointer text-ellipsis overflow-hidden whitespace-nowrap") Course Homepage
+
     .module-container(class="flex justify-center items-center p-6 sm:p-8 rounded-2xl bg-[#FFCD98] w-[90vw] sm:gap-36 shadow-lg")
       .module(
         :key="index"
-        class="flex items-center p-4 sm:p-5 bg-white rounded-xl shadow-xl transition-transform transform hover:scale-105 duration-300"
+        class="flex items-center p-4 w-fit sm:p-5 bg-white rounded-xl shadow-xl transition-transform transform hover:scale-105 duration-300"
       )
         img(src="/home.svg" alt="Reading Huddle Logo" class="w-16 h-16 mr-4 rounded-full object-contain cursor-pointer")
-        h2(class="text-4xl sm:text-2xl lg:text-4xl font-semibold text-gray-800 hover:text-yellow-500 transition-all duration-300 cursor-pointer") About Us
+        h2(class="text-2xl sm:text-4xl font-semibold text-gray-800 hover:text-yellow-500 transition-all duration-300 cursor-pointer text-ellipsis") About Us
       .module(
         :key="index"
         class="flex items-center p-4 sm:p-5 bg-white rounded-xl shadow-xl transition-transform transform hover:scale-105 duration-300"
       )
         img(src="/bookstack.svg" alt="Reading Huddle Logo" class="w-16 h-16 mr-4 rounded-full object-contain")
-        h2(class="text-4xl sm:text-2xl lg:text-4xl font-semibold text-gray-800 hover:text-yellow-500 transition-all duration-300 cursor-pointer") Course Homepage
-      .module(
-        :key="index"
-        class="flex items-center p-4 sm:p-5 bg-white rounded-xl shadow-xl transition-transform transform hover:scale-105 duration-300"
-      )
-        img(src="/bookstack.svg" alt="Reading Huddle Logo" class="w-16 h-16 mr-4 rounded-full object-contain cursor-pointer")
-        h2(class="text-4xl sm:text-2xl lg:text-4xl font-semibold text-gray-800 hover:text-yellow-500 transition-all duration-300 cursor-pointer") container 3
+        h2(class="text-2xl sm:text-4xl font-semibold text-gray-800 hover:text-yellow-500 transition-all duration-300 cursor-pointer") Course Homepage
+      
+      //- .module(
+      //-   :key="index"
+      //-   class="flex items-center p-4 sm:p-5 bg-white rounded-xl shadow-xl transition-transform transform hover:scale-105 duration-300"
+      //- )
+      //-   img(src="/bookstack.svg" alt="Reading Huddle Logo" class="w-16 h-16 mr-4 rounded-full object-contain cursor-pointer")
+      //-   h2(class="text-4xl sm:text-2xl lg:text-4xl font-semibold text-gray-800 hover:text-yellow-500 transition-all duration-300 cursor-pointer") container 3
 
     Footer
 </template>
