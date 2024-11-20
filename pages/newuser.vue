a6ec3151e6dfab5511427b7d35f3e8287010f27f
Jonathan Kopuri <kjernest01@gmail.com>
2024-11-20 11:18:15 -0600

    updated newuser form fields and images

--- pages/newuser.vue
+++ pages/newuser.vue
@@ -13,27 +13,33 @@
           
           // First Name Field
           .form-element.flex.flex-col
-            label(for="first-name" class="text-lg font-semibold text-gray-800 mb-2") First Name
+            label(for="first-name" class="text-lg font-semibold text-gray-800 mb-2" required) First Name
             input#first-name(type="text" placeholder="First Name" required class="p-3 text-base border border-gray-300 rounded-sm transition-all duration-300 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-customBlue-500")
           
           // Last Name Field
           .form-element.flex.flex-col
-            label(for="last-name" class="text-lg font-semibold text-gray-800 mb-2") Last Name
+            label(for="last-name" class="text-lg font-semibold text-gray-800 mb-2" required) Last Name
             input#last-name(type="text" placeholder="Last Name" required class="p-3 text-base border border-gray-300 rounded-sm transition-all duration-300 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-customBlue-500")
           
           // Username Field
           .form-element.flex.flex-col
-            label(for="username" class="text-lg font-semibold text-gray-800 mb-2") User Name
+            label(for="username" class="text-lg font-semibold text-gray-800 mb-2" required) User Name
             input#username(type="text" placeholder="User Name" required class="p-3 text-base border border-gray-300 rounded-sm transition-all duration-300 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-customBlue-500")
           
-          // Password Field
+          // Preferred Field
           .form-element.flex.flex-col
-            label(for="password" class="text-lg font-semibold text-gray-800 mb-2") Password
-            input#password(type="password" placeholder="Password" required class="p-3 text-base border border-gray-300 rounded-sm transition-all duration-300 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-customBlue-500")
+            label(for="preferredname" class="text-lg font-semibold text-gray-800 mb-2" optional) Preferred Name
+            input#preferredname(type="text" placeholder="Preferred Name" required class="p-3 text-base border border-gray-300 rounded-sm transition-all duration-300 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-customBlue-500")
           
-          .div(class="flex flex-col space-y-6") 
-            label(class="block text-xl font-semibold") Sign me up as a ...
-            .div(class="flex flex-row space-x-12") 
+          // Email Field
+          .form-element.flex.flex-col
+            label(for="email" class="text-lg font-semibold text-gray-800 mb-2" required) Email
+            input#email(type="text" placeholder="Email" required class="p-3 text-base border border-gray-300 rounded-sm transition-all duration-300 ease-in-out focus:border-blue-500 focus:ring-2 focus:ring-customBlue-500")
+          
+          // Assigned Role
+          .div(class="flex items-center justify-center  flex-col space-y-3") 
+            label(class="block text-xl font-semibold") Assign role as
+            .div(class="flex items-baseline space-x-12") 
               .radio-button-group(class="flex items-center space-x-8")
                 .radio-button(class="flex items-center space-x-4")
                   input(type="radio" id="dual_lang_faculty" name="dual_lang_group" value="faculty" class="h-6 w-6 text-indigo-600 focus:ring-indigo-500")
@@ -41,21 +47,6 @@
                 .radio-button(class="flex items-center space-x-4")
                   input(type="radio" id="dual_lang_parent" name="dual_lang_group" value="parent" class="h-6 w-6 text-indigo-600 focus:ring-indigo-500")
                   label(for="dual_lang_parent" class="text-lg font-medium text-gray-700 hover:text-indigo-600") Parent
-
-
-          //.div(class="flex flex-col space-y-6") 
-            label(class="block text-lg font-semibold") I am a ...
-            .div(class="flex flex-row space-x-10") 
-              .radio-button-group(class="flex items-center space-x-6")
-                .radio-button(class="flex items-center space-x-2")
-                  input(type="radio" id="dual_lang_faculty" name="dual_lang_group" value="faculty" class="h-5 w-5 text-indigo-600 focus:ring-indigo-500")
-                  label(for="dual_lang_faculty" class="text-sm font-medium text-gray-700 hover:text-indigo-600") Faculty
-                .radio-button(class="flex items-center space-x-2")
-                  input(type="radio" id="dual_lang_parent" name="dual_lang_group" value="parent" class="h-5 w-5 text-indigo-600 focus:ring-indigo-500")
-                  label(for="dual_lang_parent" class="text-sm font-medium text-gray-700 hover:text-indigo-600") Parent
-
-
-          
 
         // Submit Button
         .button-container(class="flex justify-center mt-5")
