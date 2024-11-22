02053389c84c5c46064072f4eac2e5c2e52e3b4e
Ofek Shaltiel <72337187+OfekShaltiel-UTD@users.noreply.github.com>
2024-11-22 11:06:25 -0600

    [HOTFIX] Reinstate tailwind.config.js from Jonathan's Branch

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      fill: ['hover', 'focus'], // Enables `hover` and `focus` variants for `fill`
+      stroke: ['hover', 'focus'],
+      colors: {
+        customBlue: '#2d3748',
+      },
+      keyframes: {
+        slideFadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(30px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        zoomIn: {
+          '0%': { transform: 'scale(1)', opacity: '1' },
+          '50%': { transform: 'scale(1.1)', opacity: '1' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        },
+        typing: {
+          '0%': { width: '0', borderColor: 'transparent' },
+          '100%': { width: '100%', borderColor: 'transparent' },
+        },
+      },
+      animation: {
+        slideFadeIn: 'slideFadeIn 0.8s ease-out forwards',
+        zoomIn: 'zoomIn 8s ease-in-out infinite',
+        typing: 'typing 4s steps(1000) 1s',
+      },
+    },
+  },
+  plugins: [],
+}
