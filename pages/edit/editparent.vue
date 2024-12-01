a5eda806b0b01e7282f2052bbecf9300a244ccbc
Duc Nguyen <ductrungnguyen20034@gmail.com>
2024-12-01 11:46:46 -0600

    fixed parent

--- pages/edit/editparent.vue
+++ pages/edit/editparent.vue
@@ -1,4 +1,4 @@
-<template>
+<template lang="">
     <div class="content-center">
       <!-- Submission form for editing parent profile -->
       <div class="container content-center" id="form" style="margin: 0 auto">
@@ -11,42 +11,42 @@
           <div class="sm:col-span-3 ml-28">
             <label for="zipcode" class="block text-lg font-medium leading-6 text-gray-900">Zipcode</label>
             <div class="mt-2">
-              <input v-model="parent.zipcode" type="number" name="zipcode" id="zipcode" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
+              <input v-model='parent.zipcode' type="number" name="zipcode" id="zipcode" placeholder="(user defined)" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
             </div>
           </div>
           <!-- Yearly Income -->
           <div class="sm:col-span-3 mr-28">
             <label for="yearly-income" class="block text-lg font-medium leading-6 text-gray-900">Yearly Income</label>
             <div class="mt-2">
-              <input v-model="parent.yearly_income" type="text" name="yearly-income" id="yearly-income" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
+              <input v-model='parent.yearly_income' type="text" name="yearly-income" id="yearly-income" placeholder="(user defined)" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
             </div>
           </div>
           <!-- Birth Date -->
           <div class="sm:col-span-3 ml-28">
             <label for="birth-date" class="block text-lg font-medium leading-6 text-gray-900">Birth Date</label>
             <div class="mt-2">
-              <input v-model="parent.birth_date" type="date" name="birth-date" id="birth-date" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
+              <input v-model='parent.birth_date' type="date" name="birth-date" id="birth-date" placeholder="(user defined)" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
             </div>
           </div>
           <!-- Average Number of Books Read to Child -->
           <div class="sm:col-span-3 mr-28">
             <label for="average_number_books" class="block text-lg font-medium leading-6 text-gray-900">Avg. Number of Books Read to Child</label>
             <div class="mt-2">
-              <input v-model="parent.average_number_books" type="number" name="average_number_books" id="average_number_books" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
+              <input v-model='parent.average_number_books' type="number" name="average_number_books" id="average_number_books" placeholder="(user defined)" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
             </div>
           </div>
           <!-- Phone Number -->
           <div class="sm:col-span-3 mr-28">
             <label for="phone-number" class="block text-lg font-medium leading-6 text-gray-900">Phone Number</label>
             <div class="mt-2">
-              <input v-model="parent.phone_number" type="tel" name="phone-number" id="phone-number" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
+              <input v-model='parent.phone_number' type="tel" name="phone-number" id="phone-number" placeholder="(user defined)" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
             </div>
           </div>
           <!-- Gender -->
           <div class="sm:col-span-3 ml-28">
             <label for="gender" class="block text-lg font-medium leading-6 text-gray-900">Gender</label>
             <div class="mt-2">
-              <select v-model="parent.gender" name="gender" id="gender" class="block w-full bg-gray-200 text-gray-700 border rounded-md py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white">
+              <select v-model='parent.gender' name="gender" id="gender" class="block w-full bg-gray-200 text-gray-700 border rounded-md py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white">
                 <option disabled value="">Please select gender</option>
                 <option value="Male">Male</option>
                 <option value="Female">Female</option>
@@ -58,7 +58,7 @@
           <div class="sm:col-span-3 mr-28">
             <label for="marital-status" class="block text-lg font-medium leading-6 text-gray-900">Marital Status</label>
             <div class="mt-2">
-              <select v-model="parent.marital_stat" name="marital-status" id="marital-status" class="block w-full bg-gray-200 text-gray-700 border rounded-md py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white">
+              <select v-model='parent.marital_stat' name="marital-status" id="marital-status" class="block w-full bg-gray-200 text-gray-700 border rounded-md py-2 px-3 mb-3 leading-tight focus:outline-none focus:bg-white">
                 <option disabled value="">Please select marital status</option>
                 <option value="Single">Single</option>
                 <option value="Married">Married</option>
@@ -71,32 +71,32 @@
           <div class="sm:col-span-3 ml-28">
             <label for="first-name" class="block text-lg font-medium leading-6 text-gray-900">First Name</label>
             <div class="mt-2">
-              <input v-model="parent.first_name" type="text" name="first-name" id="first-name" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
+              <input v-model='parent.first_name' type="text" name="first-name" id="first-name" placeholder="(user defined)" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
             </div>
           </div>
           <!-- Last Name -->
           <div class="sm:col-span-3 mr-28">
             <label for="last-name" class="block text-lg font-medium leading-6 text-gray-900">Last Name</label>
             <div class="mt-2">
-              <input v-model="parent.last_name" type="text" name="last-name" id="last-name" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
+              <input v-model='parent.last_name' type="text" name="last-name" id="last-name" placeholder="(user defined)" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
             </div>
           </div>
           <!-- Email -->
           <div class="sm:col-span-3 ml-28">
             <label for="email" class="block text-lg font-medium leading-6 text-gray-900">Email</label>
             <div class="mt-2">
-              <input v-model="parent.email" type="email" name="email" id="email" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
+              <input v-model='parent.email' type="email" name="email" id="email" placeholder="(user defined)" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
             </div>
           </div>
           <!-- Social Media -->
           <div class="sm:col-span-3 mr-28">
             <label for="social-media" class="block text-lg font-medium leading-6 text-gray-900">Social Media</label>
             <div class="mt-2">
-              <input v-model="parent.social_media" type="text" name="social-media" id="social-media" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
+              <input v-model='parent.social_media' type="text" name="social-media" id="social-media" placeholder="(user defined)" class="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-lg sm:leading-6">
             </div>
           </div>
           <!-- Submit Button -->
-          <button type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-60 mx-72" @click.prevent="editParent(parent)">Apply Edits</button>
+          <button type="button" class="rounded-md bg-indigo-600 px-3 py-2 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 w-60 mx-72" @click="editParent(parent)">Apply Edits</button>
         </div>
       </div>
     </div>
