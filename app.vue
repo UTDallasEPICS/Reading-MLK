a7a89e763f1259929f22a1f49c79e672c1d6b942
Tamanna Khurana <Tamanna122404@gmail.com>
2025-03-30 18:27:00 -0500

    Added sidebar with similar formatting to desired one

--- app.vue
+++ app.vue
@@ -3,6 +3,8 @@
 //import ButtonCompoent from './components/buttonComponent.vue';
 import Navbar from './components/Navbar.vue'
 import CoursePageNavbar from './components/CoursePageNavbar.vue'
+import CoursePageSidebar from './components/CoursePageSidebar.vue'
+
 import {useRoute } from 'vue-router'
 import { computed } from 'vue'
 
@@ -13,7 +15,7 @@ const isCoursePage = computed(() => route.meta.coursePage == true)
 </script>
 <template lang="pug">
 component(v-bind:is="isCoursePage ? CoursePageNavbar : Navbar")
-
+component(v-if="isCoursePage" :is="CoursePageSidebar")
 NuxtPage
-Footer
+component(v-if="!isCoursePage" :is="Footer")
 </template>
---
+++ components/CoursePageSidebar.vue
@@ -0,0 +1,15 @@
+<script setup>
+const testClick = () => {
+  console.log('Dashboard button clicked!')
+}
+</script>
+
+<template lang="pug">
+aside.sidebar(class="bg-green-700 w-64 flex flex-col items-start px-6 py-6 mt-4 mb-8 rounded-lg shadow-md")
+  h2(class="text-white text-2xl font-semibold mb-4") Dashboard
+
+  button(
+    @click="testClick"
+    class="bg-white text-green-700 font-bold py-2 px-4 rounded-md hover:bg-green-100 transition duration-300"
+  ) Test Button
+</template>
