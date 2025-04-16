2e099d3d420b6e84a2fa29cfab8ab7edd5962151
Snigdha Tadi <Snigdha.Tadi375@gmail.com>
2025-04-16 16:39:02 -0500

    Multiple choice quiz page added

--- pages/course_pages/coursehomepage.vue
+++ pages/course_pages/coursehomepage.vue
@@ -27,6 +27,12 @@ const message = ref('Welcome to the Course Home Page!')
           class="inline-block mt-6 px-6 py-3 bg-customBlue text-white rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300"
         ) Start Questions
 
+        // link to Multiple Choice Questions page (multiplechoicequiz)
+        router-link(
+          to="multiplechoicequiz"
+          class="inline-block mt-6 px-6 py-3 bg-customBlue text-white rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300"
+        ) Start Multiple Choice Quiz
+
     </template>
     
 
