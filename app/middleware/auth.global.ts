07df794f5246f33b3097adaf1baba11d903b7ff6
chanabyte <goldilocks.047@gmail.com>
2026-03-18 19:21:37 -0500

    reader updates

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -1,15 +1,19 @@
-import { authClient } from '../utils/auth-client'
+// import { authClient } from '../utils/auth-client'
 
-export default defineNuxtRouteMiddleware(async (to) => {
-  const { data: session } = await authClient.useSession(useFetch)
-
-  if (session.value) {
-    if (to.path === '/auth') {
-      return navigateTo('/')
-    }
-  } else {
-    if (to.path !== '/auth') {
-      return navigateTo('/auth')
-    }
-  }
+export default defineNuxtRouteMiddleware(() => {
+  // auth disabled for now
 })
+
+// export default defineNuxtRouteMiddleware(async (to) => {
+//   const { data: session } = await authClient.useSession(useFetch)
+
+//   if (session.value) {
+//     if (to.path === '/auth') {
+//       return navigateTo('/')
+//     }
+//   } else {
+//     if (to.path !== '/auth') {
+//       return navigateTo('/')
+//     }
+//   }
+// })
