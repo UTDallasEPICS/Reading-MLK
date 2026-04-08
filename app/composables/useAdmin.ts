2625944ccdf6ae2d298cb77f215888a2b00e6e13
chanabyte <goldilocks.047@gmail.com>
2026-04-08 00:26:57 -0500

    api endpoints for form added

--- app/composables/useAdmin.ts
+++ app/composables/useAdmin.ts
@@ -111,7 +111,7 @@ export const useAdmin = () => {
   }
   const getLastMonday = (dateStr: string) => {
     const d = new Date(`${dateStr}T00:00:00Z`)
-    const daysSinceMonday = (d.getDay() + 6) % 7
+    const daysSinceMonday = (d.getDay()) % 7
     d.setDate(d.getDate() - daysSinceMonday)
     return d.toISOString().slice(0, 10)
   }
