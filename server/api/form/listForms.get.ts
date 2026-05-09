f282ce84f92c2c03ddb7320113a6c9d51402c438
AidanLoran <asloran23@gmail.com>
2026-05-08 22:04:50 -0500

    initial

---
+++ server/api/form/listForms.get.ts
@@ -0,0 +1,9 @@
+import { prisma } from '../../utils/prisma'
+import { getQuery, createError } from 'h3'
+import { requireAdmin, requireSession } from '../../utils/require-session'
+
+
+export default defineEventHandler(async (event) => {
+  
+
+  })
