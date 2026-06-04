5253f7dbe60364bbdca825967ddd13c916b6645f
AidanLoran <asloran23@gmail.com>
2026-06-04 14:46:41 -0500

    fixed boolean coercion issue for form/list.get

--- server/api/form/list.get.ts
+++ server/api/form/list.get.ts
@@ -3,25 +3,27 @@ import { Prisma } from '~~/prisma/generated/client'
 import { getQuery, createError } from 'h3'
 import { requireAdmin, requireSession } from '../../utils/require-session'  
 import z from 'zod'
-import { formInclude } from '../../utils/inclusions'
+import { formInclude } from '../../utils/prismaInclusions'
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
-  //requireSessions
-  //add error handling
+  //add requireSessions
   const formGroupId = z.coerce.number().safeParse(query.formGroupId).data
   const weeklyDate = z.coerce.date().safeParse(query.weeklyDate).data
-  const published = z.boolean().safeParse(query.published).data
-
+  
   const where: Prisma.FormWhereInput = {}
 
-  console.log("\npublished after filtering", published)
-
-  if (published !== undefined) {where.published = published}
+  //expecting 0/1 for t/f
+  if (query.published) {
+    const pre = z.coerce.number().safeParse(query.published)
+    if (!pre.success) { throw createError({ statusCode: 400, message: 'Invalid published value' })}
+    const published = z.coerce.boolean().safeParse(pre.data).data
+    where.published = published
+  }
 
   if (formGroupId) {where.formGroup = formGroupId}
 
-  else if (weeklyDate) {
+  if (weeklyDate) {
     const matchingGroup = await $fetch('/api/formGroup/matchByDate', {
       query: { date: weeklyDate.toISOString() }
     })
@@ -35,7 +37,6 @@ export default defineEventHandler(async (event) => {
       ? { gte: matchingGroup.startDate, lte: matchingGroup.endDate }
       : { gte: matchingGroup.startDate }
   }
-
 
   const forms = await prisma.form.findMany({
     where,
