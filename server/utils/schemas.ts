6ad5794e9099efd5b3488125deaa969bd589ae18
Mateo <mci230001@utdallas.edu>
2026-06-11 11:02:39 -0500

    settingsFix

--- server/utils/schemas.ts
+++ server/utils/schemas.ts
@@ -18,7 +18,7 @@ export const studentCreateSchema = z.object({
   name: z.string("Invalid Name").min(1).max(35)
 })
 
-export const studentUpdateSchema = studentCreateSchema.extend({
+export const studentUpdateSchema = studentCreateSchema.partial().extend({
   settings: z.object({
     dyslexiaFont: z.boolean().optional(), 
     fontSize: z.number().min(1).max(1.5).optional(),
